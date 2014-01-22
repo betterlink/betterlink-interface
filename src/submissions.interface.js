37ec400ec49e7bcc2582d2ec19b65347c2df4d42
betson <ben.getson@gmail.com>
2014-01-22 00:02:18 -0500

    spike of in-place interface to submit selections
    The basic interface gets out of the way of the user: provide 'bookends'
    that enclose the range that could be submitted, but otherwise don't show
    anything. On hover, identify that you can click on the selected content
    to submit a new link.
    
    Open issues:
    - how to select content that is currently decorated
    - use a different solution for :hover (because the hover only applies to
    individual elements for a multi-piece range)
    - submit the previous selection instead of relying on currentSelection()
    - don't remove the selection after applying the 'prospectiveSubmission'
    decoration
    - create a loading animation
    - allow Submissions.Result to cancel the loading animation
    - choose a CSS style for the bookends

--- src/submissions.interface.js
+++ src/submissions.interface.js
@@ -3,45 +3,75 @@
  *
  */
 betterlink_user_interface.createModule("Submissions.Interface", function(api, apiInternal) {
-	api.requireModules( ["Util.DOM", "Util.Ranges", "Submissions", "Event Messaging"] );
-
-	var SELECTION_DIV_ID = "betterlink_selection";
-	var SUBMIT_BUTTON_ID = "betterlink_selection_button";
-	var SUBMIT_BUTTON_TEXT = "Share Selection";
+	api.requireModules( ["Util", "Util.DOM", "Util.Ranges", "Submissions", "Selection Highlighter", "Event Messaging"] );
 
 	var LOADING_MESSAGE = "Generating your link...";
+	var HIGHLIGHTER_ID_PREFIX = "prospectiveSubmission";
 
-	var SELECTION_CSS = ["{  margin: 2px;",
-							"padding: 3px;",
-							"width: auto;",
-							"border: 1px solid black;",
-							"border-radius: 1em;",
-							"background-color: lightskyblue; }"].join(' ');
+	var PROSPECTIVE_SUBMISSION_CSS_CLASS = "betterlink-prospective-submission";
+	var PROSPECTIVE_SUBMISSION_CSS = "." + PROSPECTIVE_SUBMISSION_CSS_CLASS + ":hover" +
+							[" { background: #DADADA;",
+								"text-decoration: underline;",
+								"cursor: pointer; }"].join(' ');
 
-	var SUBMIT_BUTTON_CSS = ["{  color: #333;",
-								"font-size: 14px;",
-								"font-weight: bold;",
-								"font-family: Arial, Helvetica, sans-serif;",
-								"line-height: initial;",
-								"margin: 2px;",
-								"padding: 2px 6px;",
-								"text-shadow: none;",
-								"box-shadow: none;",
-								"border: 2px outset buttonface;",
-								"border-radius: initial;",
-								"background: buttonface; }"].join(' ');
+	// ********************   ARROW CSS   ********************
+	var ARROW_CSS = [" { position: absolute;",
+						"width: 0;",
+						"height: 0;",
+						"margin-top: -5px;",
+						"margin-left: -5px;",
+						"border-left: 5px solid transparent;",
+						"border-right: 5px solid transparent;",
+						// ie6 height fix
+						"font-size: 0;",
+						"line-height: 0;",
+						// ie6 transparent fix
+						"_border-right-color: pink;",
+						"_border-left-color: pink;",
+						"_filter: chroma(color=pink); }"].join(' ');
 
-	var selectionDiv;
-	var selectionDivSubmissionButton;
-	var urlDiv;
-	var previousMousePosition; // set on each mousedown event
-	apiInternal.submissions.interface = {
-		updateSubmissionDiv: updateSubmissionDiv,
-		resetSubmissionDiv: resetSubmissionDiv
-	};
+	var ARROW_DOWN_CSS_CLASS = "betterlink-arrow-down";
+	var ARROW_DOWN_CSS = ARROW_CSS.replace("{", "{ border-top: 5px solid #4169E1;");
+
+	var ARROW_UP_CSS_CLASS = "betterlink-arrow-up";
+	var ARROW_UP_CSS = ARROW_CSS.replace("{", "{ border-bottom: 5px solid #4169E1;")
+								.replace(/margin-top:[^;]*;/, "margin-top: inherit;");
+
+	// ******************** L-Shape CSS ********************
+	var l_shared_css = [" { position: absolute;",
+					"width: 12px;",
+					"height: 5px;",
+					// ie6 height fix
+					"font-size: 0;",
+					"line-height: 0; }"].join(' ');
+
+	var l_top_addition =      [ "margin-top: -5px;",
+								"border-top: 2px solid #4169E1;",
+								"border-left: 2px solid #4169E1;"].join(' ');
+
+	var l_bottom_addition =   [ "margin-top: inherit;",
+								"margin-left: -12px;",
+								"border-bottom: 2px solid #4169E1;",
+								"border-right: 2px solid #4169E1;"].join(' ');
+
+	var L_TOP_CSS_CLASS = "betterlink-l-top";
+	var L_TOP_CSS = l_shared_css.replace("{", "{ " + l_top_addition);
+
+	var L_BOTTOM_CSS_CLASS = "betterlink-l-bottom";
+	var L_BOTTOM_CSS = l_shared_css.replace("{", "{ " + l_bottom_addition);
+
+	// ************************************************************
+
+	var activeHighlighters = [];
+
+	var identifierAttributeName = "data-identifier";
+	var highlightBookends = {};
+
+	apiInternal.submissions.interface = {};
 	/****************************************************************************************************/
 
 	apiInternal.addInitListener(initializeInterface);
+	apiInternal.events.registerObserverForRemoveBetterlink(removeExistingDecorations);
 	function initializeInterface() {
 		if(apiInternal.submissions.interface.initialized) {
 			return;
@@ -49,32 +79,119 @@ betterlink_user_interface.createModule("Submissions.Interface", function(api, ap
 
 		apiInternal.submissions.interface.initialized = true;
 
-		selectionDiv = buildSelectionDiv();
-		attachMousedownEvent(function(e) { previousMousePosition = captureMousePosition(e); });
-		attachMouseupEvent(togglePositionAndDisplayOfSelectionDiv);
-		// TODO: Support keydown / keyup events for accessibility (when starting a selection)
-		// TODO: Handle when keydown event changes the selection (e.g., tabbing away from focus)
+		insertProspectiveSubmissionStyle();
+		insertBookendStyles();
+		attachMouseupEvent(toggleDisplayOfProspectiveSubmissions);
 	}
 
-	function buildSelectionDiv() {
-		insertSelectionDivStyle();
+	// The 'prospective submission' style is used to markup a selection that could
+	// be submitted to Betterlink as a new link.
+	function insertProspectiveSubmissionStyle() {
+		apiInternal.util.dom.createAndAppendStyleElement(PROSPECTIVE_SUBMISSION_CSS);
+	}
 
-		var selectionDiv = document.createElement("div");
-		selectionDiv.id = SELECTION_DIV_ID;
-		selectionDiv.style.position = "absolute";
-		hideSelectionDiv(selectionDiv);
+	// Bookends will be added to the DOM to enclose the prospective submission, so
+	// that it is clear to the user that they can interact with the content there.
+	function insertBookendStyles() {
+		apiInternal.util.dom.addCssByClass(ARROW_DOWN_CSS_CLASS, ARROW_DOWN_CSS);
+		apiInternal.util.dom.addCssByClass(ARROW_UP_CSS_CLASS, ARROW_UP_CSS);
 
-		var submitButton = document.createElement("input");
-		submitButton.type = "button";
-		submitButton.id = SUBMIT_BUTTON_ID;
-		submitButton.value = SUBMIT_BUTTON_TEXT;
-		apiInternal.addListener(submitButton, "touchstart", sendSubmission);
-		apiInternal.addListener(submitButton, "click", sendSubmission);
+		apiInternal.util.dom.addCssByClass(L_TOP_CSS_CLASS, L_TOP_CSS);
+		apiInternal.util.dom.addCssByClass(L_BOTTOM_CSS_CLASS, L_BOTTOM_CSS);
+	}
 
-		selectionDiv.appendChild(submitButton);
-		selectionDivSubmissionButton = submitButton;
-		apiInternal.util.dom.registerAndAppend(document.body, selectionDiv);
-		return selectionDiv;
+	// Triggered on a mouseup event. If the user is finishing their click with part of
+	// the document selected, then markup the document to identify the prospective
+	// Betterlink submission. Also, remove any prior submission markups.
+	function toggleDisplayOfProspectiveSubmissions() {
+		if(!selectionIsEmpty()) { // only change if something else is being selected
+			removeExistingDecorations();
+			decorateProspectiveSubmission();
+		}
+	}
+
+	// Remove any highlights that might already be on the page for previous selections
+	function removeExistingDecorations() {
+		apiInternal.util.forEach(activeHighlighters, function(highlighterName, index) {
+			apiInternal.highlighters.removeAllHighlights(highlighterName);
+
+			var highlighterId = /\|(.*)/.exec(highlighterName)[1];
+			removeSelectionBookends(highlighterId)
+		});
+		activeHighlighters.length = 0;
+	}
+
+	// Highlight the current selection to indicate what would be saved if the selection
+	// were submitted to create a new link. We append an identifier to the highlighter
+	// name so that we can distinguish the multiple selections we'll end up creating.
+	function decorateProspectiveSubmission() {
+		var uniqueIdentifier = Math.floor(1E8 * Math.random());
+		var highlighterName = HIGHLIGHTER_ID_PREFIX + "|" + uniqueIdentifier;
+		var activeSelection = apiInternal.util.ranges.getCurrentSelection();
+
+		activeHighlighters.push(highlighterName);
+		var success = createProspectiveSubmissionHighlighter(highlighterName, uniqueIdentifier);
+
+		if(success) {
+			apiInternal.highlighters.highlightSelection(highlighterName, activeSelection);
+			insertSelectionBookends(uniqueIdentifier);
+		}
+		else {
+			apiInternal.warn('There was a problem adding a highlighter to markup prospective submissions');
+		}
+	}
+
+	// Insert bookend elements before and after the area that was highlighted. These indicate
+	// the range of the highlight to the user.
+	// The start and end elements are saved via a callback function that runs when each of the
+	// elements are created.
+	function insertSelectionBookends(highlighterId) {
+		if(highlightBookends[highlighterId]) {
+			var startElement = highlightBookends[highlighterId].start;
+			var endElement = highlightBookends[highlighterId].end;
+
+			var startBookend = document.createElement('b');
+			startBookend.className = L_TOP_CSS_CLASS;
+
+			var endBookend = document.createElement('b');
+			endBookend.className = L_BOTTOM_CSS_CLASS;
+
+			apiInternal.util.dom.registerAndInsertBefore(startBookend, startElement);
+			apiInternal.util.dom.registerAndInsertAfter(endBookend, endElement);
+
+			highlightBookends[highlighterId].startBookend = startBookend;
+			highlightBookends[highlighterId].endBookend = endBookend;
+		}
+	}
+
+	// Remove the bookends associated with a particular highlighter identifier. When removing
+	// the elements from the DOM, we don't need to 'unregister' them with Betterlink. If\
+	// Betterlink needs to be detached, it will silently skip over registered elements that
+	// have already been removed from the DOM.
+	function removeSelectionBookends(highlighterId) {
+		if(highlightBookends[highlighterId]) {
+			var startBookend = highlightBookends[highlighterId].startBookend;
+			var endBookend = highlightBookends[highlighterId].endBookend;
+
+			if(startBookend) {
+				if(startBookend.parentNode) {
+					startBookend.parentNode.removeChild(startBookend);
+				}
+			}
+			if(endBookend) {
+				if(endBookend.parentNode) {
+					endBookend.parentNode.removeChild(endBookend);
+				}
+			}
+			delete highlightBookends[highlighterId];
+		}
+	}
+
+	// Submit the selection to the server
+	function sendSubmission() {
+		// displayLoadingMessage();
+		apiInternal.events.fireNewSubmission(); // needs to pass the previous selection
+		removeExistingDecorations();
 	}
 
 	function attachMousedownEvent(eventName) {
@@ -85,99 +202,82 @@ betterlink_user_interface.createModule("Submissions.Interface", function(api, ap
 		apiInternal.addListener(document, "mouseup", eventName);
 	}
 
-	function togglePositionAndDisplayOfSelectionDiv(e) {
-		// BUG: If I click ontop of already-selected text, the selection disappears,
-		//      but the button still remains visible
-		// TODO: Don't do anything if the mouse is ontop of the <div>
-		if(selectionIsNotEmpty()) {
-			var mousePosition = captureMousePosition(e);
-			var movingDown = (mousePosition.Y - previousMousePosition.Y) >= 0;
-			
-			var divPosition = getSelectionDivPosition(mousePosition, movingDown);
-			showSelectionDiv(selectionDiv, divPosition);
+	// Return if the document has any text that is currently selected
+	function selectionIsEmpty() {
+		return apiInternal.util.ranges.currentSelectionIsEmpty();
+	}
+
+	// Create a new Highlighter (referenced by a provided identifier) and register
+	// it with Betterlink. If successful, will return the highlighter identifier.
+	// Otherwise, will return false.
+	function createProspectiveSubmissionHighlighter(identifier, highlighterId) {
+		// Define this up here so that we can call the element attribute by
+		// reference (so that we only need to define it once). For the sake
+		// of JS compilers, we need to ensure the object keys are referenced
+		// by string, instead of by symbol.
+		var elementAttributes = {};
+		elementAttributes[identifierAttributeName] = highlighterId;
+
+		var highlightOptions = {
+
+			// any element that's highlighted should have the following properties
+			'elementProperties': {
+				'title': 'Click to create your personal link'
+			},
+
+			// any element that's highlighted should have these non-standard
+			// attributes
+			'elementAttributes': elementAttributes,
+
+			// elements that we will apply our CSS class to, instead of creating
+			// a new container element. ex:
+			// <span class="myclass">this is my text</span> v.
+			// <span><mark class="myclass">this is my text</mark></span>
+			//
+			// Note: if the existing element doesn't have all of the properties
+			// specified above, we'll create a new container element anyways.
+			'tagsToPreserve': ['span'],
+
+			// element type that we will wrap around the selected content when
+			// splitting text nodes or when we can't apply our class name to
+			// an existing element
+			'elementTagName': 'span',
+
+			// CSS class name that will be applied to each element that is
+			// highlighted
+			'cssClass': PROSPECTIVE_SUBMISSION_CSS_CLASS,
+
+			// Callback function that will get executed for each HTML element
+			// that is created. Will be passed a single parameter for the element
+			// created.
+			'onElementCreate': decorationCallback
+		};
+
+		return apiInternal.highlighters.add(identifier, highlightOptions);
+	}
+
+	// Executed for each element that is created during the decoration process
+	function decorationCallback(element) {
+		var identifier = element.getAttribute(identifierAttributeName);
+		if(identifier) {
+			storeBookends(identifier, element);
+		}
+
+		apiInternal.addListener(element, "touchstart", sendSubmission);
+		apiInternal.addListener(element, "click", sendSubmission);
+	}
+
+	// Store references to the first and last HTML elements that are created. The
+	// intent is that we'll use these references to place our bookend elements
+	function storeBookends(identifier, mostRecentElement) {
+		if(highlightBookends[identifier]) {
+			highlightBookends[identifier].end = mostRecentElement;
 		}
 		else {
-			hideSelectionDiv(selectionDiv);
+			highlightBookends[identifier] = {
+				start: mostRecentElement,
+				end: mostRecentElement
+			};
 		}
-	}
-
-	// return the mouse position in relation to the absolute layout
-	function captureMousePosition(e) {
-		// reference on viewports:
-		// http://www.quirksmode.org/mobile/viewports2.html
-
-		// Cross-Compatibility for IE
-		var scrollX = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
-		var scrollY = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-
-		/**
-		 * re:Mobile:
-		 * clientX/Y - coordinates relative to the visual viewport (what's displayed)
-		 * pageX/Y - coordinates relative to the layout viewport (what's rendered)
-		 * The difference is based on how zoomed in you are and where you've scrolled to.
-		 * 
-		 * Note: IE8< doesn't support pageX/Y
-		 */
-		return { X: e.clientX + scrollX, Y: e.clientY + scrollY };
-	}
-
-	// Update the position of the div to generally get out of the way of whatever
-	// content is selected. We approximate this by checking which direction the user
-	// was selecting content.
-	function getSelectionDivPosition(mousePosition, movingDown) {
-		var yOffset, xOffset;
-		var pixelBuffer = 5;
-		if(movingDown) {
-			yOffset = pixelBuffer;
-			xOffset = pixelBuffer;
-		}
-		else {
-			yOffset = -1 * (selectionDiv.offsetHeight + pixelBuffer);
-			xOffset = -1 * (selectionDiv.offsetWidth + pixelBuffer);
-		}
-		
-		return { X: mousePosition.X + xOffset, Y: mousePosition.Y + yOffset };
-	}
-
-	function showSelectionDiv(div, divPosition) {
-		div.style.display = "";
-		div.style.left = divPosition.X + "px";
-		div.style.top = divPosition.Y + "px";
-	}
-
-	function hideSelectionDiv(div) {
-		div.style.display = "none";
-		div.style.left = "-999em";
-	}
-
-	function selectionIsNotEmpty() {
-		return !apiInternal.util.ranges.currentSelectionIsEmpty();
-	}
-
-	function insertSelectionDivStyle() {
-		apiInternal.util.dom.addCssById(SELECTION_DIV_ID, SELECTION_CSS);
-		apiInternal.util.dom.addCssById(SUBMIT_BUTTON_ID, SUBMIT_BUTTON_CSS);
-	}
-
-	function sendSubmission() {
-		displayLoadingMessage();
-		apiInternal.events.fireNewSubmission();
-	}
-
-	function displayLoadingMessage() {
-		var span = document.createElement("span");
-		span.appendChild(document.createTextNode(LOADING_MESSAGE));
-		updateSubmissionDiv(span);
-	}
-
-	// Update the 'selectionDiv' to contain the provided element
-	// Will replace any existing elements contained within the div
-	function updateSubmissionDiv(newElement) {
-		apiInternal.util.dom.addOrReplaceChild(selectionDiv, newElement);
-	}
-
-	// Reset the 'selectionDiv' to contain its original elements
-	function resetSubmissionDiv() {
-		updateSubmissionDiv(selectionDivSubmissionButton);
 	}
 });
