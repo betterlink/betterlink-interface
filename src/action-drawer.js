776f7dfe2a82bc506eaab0f1a26b7da7cef0ceb7
betson <ben.getson@gmail.com>
2014-05-09 19:07:46 -0400

    re-layout drawer for top and bottom core sections
    Updates all classes to have the 'betterlink-' prefix. Also creates two
    <div>s in the center section: one that flows downward and another that
    flows upward from the bottom.

--- src/action-drawer.js
+++ src/action-drawer.js
@@ -9,8 +9,11 @@ betterlink_user_interface.createModule("Action Drawer", function(api, apiInterna
 
 	var HTML5_CSS = "article, aside, footer, header, nav, section { display: block; }";
 	var DRAWER_ID = "betterlink-drawer";
+	var DRAWER_HEADER_ID = DRAWER_ID + "-header";
+	var DRAWER_CENTER_ID = DRAWER_ID + "-center";
+	var DRAWER_FOOTER_ID = DRAWER_ID + "-footer";
+
 	var DRAWER_HIDDEN_CLASS = "betterlink-drawer-hidden";
-	var DRAWER_HIDDEN_CSS = "{ display: none; }"
 	var DROPZONES_LIST_ID = "betterlink-dropzones";
 
 	// Drawer Animation
@@ -19,22 +22,31 @@ betterlink_user_interface.createModule("Action Drawer", function(api, apiInterna
 
 	// Drawer CSS
 	var DRAWER_CSS = 
-		[   ".row, .col { overflow: hidden; position: fixed; }",
-			".row { width: 100%; position: absolute; }",
-			".col { top: 0; bottom: 0; }",
-			".scroll-x { overflow-x: auto; }",
-			".scroll-y { overflow-y: auto; }",
+		[   ".betterlink-row, .betterlink-col { overflow: hidden; position: absolute; }",
+			".betterlink-row { width: 100%; }",
+			".betterlink-col { top: 0; bottom: 0; }",
+			".betterlink-scroll-x { overflow-x: auto; }",
+			".betterlink-scroll-y { overflow-y: auto; }",
 
-			".left.col { width: 230px; }",
-			".right.col { width: 230px; right: 0; }",
-			".header.row { height: 75px; line-height: 75px; text-align: center; border-bottom: 1px solid black; }",
-			".body.row { top: 75px; bottom: 50px; }",
-			".footer.row { height: 50px; bottom: 0; line-height: 50px; text-align: center; border-top: 1px solid black; }",
+			".betterlink-left.betterlink-col { width: 230px; }",
+			".betterlink-right.betterlink-col { width: 230px; right: 0; }",
+
+			".betterlink-header.betterlink-row { height: 75px; line-height: 75px; }",
+			".betterlink-center.betterlink-row { top: 75px; bottom: 50px; }",
+			".betterlink-footer.betterlink-row { height: 50px; bottom: 0; line-height: 50px; }",
+
+			".betterlink-top-to-bottom { width: 100%; }",
+			".betterlink-bottom-to-top { bottom: 0; position: absolute; width: 100%; }",
+			"div.betterlink-reset { padding: 0; margin: 0; border: none; background: inherit; border-radius: 0; }",
+
+			"#" + DRAWER_HEADER_ID + " { text-align: center; border-bottom: 1px solid black; }",
+			"#" + DRAWER_FOOTER_ID + " { text-align: center; border-top: 1px solid black; }",
 
 			"#" + DROPZONES_LIST_ID + " { margin: 0; padding: 0; }",
 			"#" + DROPZONES_LIST_ID + ">li { list-style: none; }",
 
-			"#" + DRAWER_ID + " { background: lightcoral; }"].join(' ');
+			"#" + DRAWER_ID + " { background: lightcoral; position: fixed; }",
+			"." + DRAWER_HIDDEN_CLASS + " { display: none; }"].join(' ');
 
 	var drawer;
 	apiInternal.drawer = {
@@ -54,7 +66,6 @@ betterlink_user_interface.createModule("Action Drawer", function(api, apiInterna
 	function insertDrawerStyles() {
 		apiInternal.util.dom.createAndAppendStyleElement(HTML5_CSS);
 		apiInternal.util.dom.createAndAppendStyleElement(DRAWER_CSS);
-		apiInternal.util.dom.addCssByClass(DRAWER_HIDDEN_CLASS, DRAWER_HIDDEN_CSS);
 	}
 
 	// Display the drawer to the user
@@ -77,22 +88,33 @@ betterlink_user_interface.createModule("Action Drawer", function(api, apiInterna
 	function createDrawer() {
 		drawer = document.createElement('aside');
 		drawer.id = DRAWER_ID;
-		drawer.className = 'right col';
+		drawer.className = 'betterlink-right betterlink-col';
 
 		var header = document.createElement('section');
-		header.className = 'header row';
+		header.id = DRAWER_HEADER_ID;
+		header.className = 'betterlink-header betterlink-row';
 		header.appendChild(document.createTextNode('My Header'));
 
-		var body = document.createElement('section');
-		body.className = 'body row';
-		addDropzones(body);
+		var center = document.createElement('section');
+		center.id = DRAWER_CENTER_ID;
+		center.className = 'betterlink-center betterlink-row';
+		var center_top = document.createElement('div');
+		var center_bottom = document.createElement('div');
+		center_top.className = 'betterlink-top-to-bottom betterlink-reset';
+		center_bottom.className = 'betterlink-bottom-to-top betterlink-reset';
+
+		addDropzones(center_top);
+		addDropzones(center_bottom);
+		center.appendChild(center_top);
+		center.appendChild(center_bottom);
 
 		var footer = document.createElement('section');
-		footer.className = 'footer row';
+		footer.id = DRAWER_FOOTER_ID;
+		footer.className = 'betterlink-footer betterlink-row';
 		footer.appendChild(document.createTextNode('My Footer'));
 
 		drawer.appendChild(header);
-		drawer.appendChild(body);
+		drawer.appendChild(center);
 		drawer.appendChild(footer);
 
 		hideDrawer();
