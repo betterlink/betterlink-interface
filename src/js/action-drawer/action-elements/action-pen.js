0e159ed42b422d913403f16d1d337ebe754c3d2e
betson <ben@betterlink.io>
2014-12-24 23:40:00 -0500

    support action elements with sibling elements
    In some cases, it may make sense for an Action Element to return an
    array of elements, instead of a single element. For example, a label
    could accompany an image.
    
    In these situations, each element in the array would be appended to a
    parent <li> in order.

--- src/js/action-drawer/action-elements/action-pen.js
+++ src/js/action-drawer/action-elements/action-pen.js
@@ -11,7 +11,7 @@ betterlink_user_interface.createModule("Action Pen", function(api, apiInternal) 
 
 	var CSS = apiInternal.drawerSelector + [
 		"." + PEN_CLASS + " { list-style: none; padding: 5px; margin: 0; text-align: center; }",
-		"." + PEN_CLASS + ">li { display: inline-block; padding: 0; margin: 0; }",
+		"." + PEN_CLASS + ">li { display: inline-block; padding: 0; margin: 0; vertical-align: top; }",
 		"." + ACTION_ELEMENT_CLASS + ' { cursor: pointer; padding: 5px; margin: 2px 3px; width: auto; border: none; background: inherit; border-radius: 0; }',
 		"img." + ACTION_ELEMENT_CLASS + ',' + apiInternal.drawerSelector + 'svg.' + ACTION_ELEMENT_CLASS + ' { width: 75px; height: 75px; }'
 	].join(' ' + apiInternal.drawerSelector);
@@ -39,7 +39,14 @@ betterlink_user_interface.createModule("Action Pen", function(api, apiInternal) 
 	function addElements(list) {
 		for(var i  = 0, len = sharers.length; i < len; i++) {
 			var li = document.createElement('li');
-			li.appendChild(sharers[i].create());
+
+			// Support Action Elements that return an array of elements.
+			// Each element gets added to the <li> in order.
+			var elements = sharers[i].create();
+			if(!elements.length) { elements = [elements]; }
+			for(var j = 0, jLen = elements.length; j < jLen; j++) {
+				li.appendChild(elements[j]);
+			}
 			list.appendChild(li);
 		}
 		return list;
