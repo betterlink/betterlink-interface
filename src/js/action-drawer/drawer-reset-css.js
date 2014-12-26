f537bc8565175acee9820901365dea5400920da5
betson <ben@betterlink.io>
2014-12-26 17:57:02 -0500

    fix drawer-reset structure
    The previous structure prevented children elements from naturally
    inheriting properties from their closest parents. This is because the
    selectors defined here would exactly match each child element.
    
    The problem is that we weren't properly segmenting between inherited and
    non-inherited properties. For inherited properties, we need to reset
    them by setting them all to 'inherit', then apply the desired default
    values to the top-level betterlink-drawer.

--- src/js/action-drawer/drawer-reset-css.js
+++ src/js/action-drawer/drawer-reset-css.js
@@ -24,18 +24,45 @@ betterlink_user_interface.createModule("Drawer Reset CSS", function(api, apiInte
 	var drawerSelector = "#betterlink-drawer ";
 	var elements = drawerSelector + ["div","span","h1","h2","p","article","aside","footer","header","nav","section"].join(',' + drawerSelector);
 	apiInternal.drawerResetCss =
-		[   elements +"{ border: none;",
-						"border-radius: 0;",
-						"margin: 0;",
-						"padding: 0;",
-						"vertical-align: baseline;",
+			// Reset & New Defaults: Properties that don't inherit
+		[   elements  +  " { background: transparent;",
+							"border: none;",
+							"border-radius: 0;",
+							"border-color: #BCBCBC;",
+							"clear: none;",
+							"margin: 0;",
+							"padding: 0;",
+							"vertical-align: baseline; }",
 
-						"color: #424242;",
-						"border-color: #BCBCBC;",
-						"font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; }",
+			// Reset: Inherited properties
+			elements  +  " { color: inherit;",
+							"font-family: inherit;",
+							"letter-spacing: inherit;",
+							"line-height: inherit;",
+							"text-align: inherit;",
+							"text-indent: inherit; }",
 
-			drawerSelector +  "div { background: inherit; ",
-									"color: inherit; }"].join(' ');
+			// New Defaults: Inherited properties
+			drawerSelector + " { color: #424242;",
+								"font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;",
+								"letter-spacing: normal;",
+								"line-height: normal;",
+								"text-align: left;",
+								"text-indent: 0; }"
+		].join(' ');
+
+			// New Defaults: Inherited properties (tag overrides)
+			// ....can't happen here. If we wanted to make all <div> elements
+			// center-align their text, we would break the ability to have
+			// children inherit overrides from a closer parent. This is because
+			// the {text-align:center;} would match the element exactly, and
+			// the CSS engine would not look up the tree to its parents.
+			//
+			// *However*, this trade-off can be made for elements that are not
+			// expected to have children inheriting styles. <p> and <h1> may be
+			// candidates depending on the property (because child <span>
+			// elements would not inherit that property from their closest
+			// parent).
 
 	apiInternal.drawerSelector = drawerSelector;
 });
